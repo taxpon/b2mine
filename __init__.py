add9118840fc8a365bfba32906f75f1ad2c03bf3
Takuro Wada <taxpon@gmail.com>
2016-04-03 14:25:59 +0900

    Clean up old codes

--- __init__.py
+++ __init__.py
@@ -3,7 +3,9 @@ import sys
 import time
 
 import bpy
+import bmesh
 from mathutils import Vector
+from mathutils import bvhtree as bvh
 import mathutils.geometry as mg
 
 sys.path.append(os.path.dirname(__file__))
@@ -19,103 +21,168 @@ bl_info = {
 
 class Converter(object):
 
-    @staticmethod
-    def check_if_points_exist_in_box(box, points):
-        """
-        :param list[Vector] box:
-        :param list[Vector] points:
-        :return:
-        """
-        exist = False
-        for point in points:
-            if box[0].z <= point.z < box[1].z and \
-               box[0].y <= point.y < box[3].y and \
-               box[0].x <= point.x < box[5].x:
-                exist = True
-                break
-        return exist
+    def __init__(self):
+        self.voxel_list = []
 
     @staticmethod
-    def define_grids(obj, level=6):
-        dim = obj.dimensions
-        unit = max(dim) / float(level)
+    def create_new_octree(box):
+        box0 = (
+            box[0],
+            (box[0] + box[1])/2.0,
+            (box[0] + box[2])/2.0,
+            (box[0] + box[3])/2.0,
+            (box[0] + box[4])/2.0,
+            (box[0] + box[5])/2.0,
+            (box[0] + box[6])/2.0,
+            (box[0] + box[7])/2.0,
+        )
 
-        start = Vector(obj.bound_box[0])
-        vec_list = []
+        box1 = (
+            # Left side
+            (box[0] + box[1])/2.0,
+            box[1],
+            (box[1] + box[2])/2.0,
+            (box[0] + box[2])/2.0,
+            # Right side
+            (box[0] + box[5])/2.0,
+            (box[1] + box[5])/2.0,
+            (box[1] + box[6])/2.0,
+            (box[0] + box[6])/2.0,
+        )
 
-        for dz in range(int(dim[2]/unit + 2)):
-            # for dz in [2]:
-            # for dy in range(int(dim[1]/unit + 2)):
-            #     for dx in range(int(dim[0]/unit + 1)):
-        # for dz in [0]:
-            for dy in [0]:
-                for dx in [0]:
-                    nvec = start.copy()
-                    nvec.x += dx * unit
-                    nvec.y += dy * unit
-                    nvec.z += dz * unit
-                    vec_list.append(nvec)
+        box2 = (
+            # Left side
+            (box[0] + box[2])/2.0,
+            (box[1] + box[2])/2.0,
+            box[2],
+            (box[2] + box[3])/2.0,
+            # Right side
+            (box[0] + box[6])/2.0,
+            (box[1] + box[6])/2.0,
+            (box[2] + box[6])/2.0,
+            (box[3] + box[6])/2.0
+        )
 
-        return vec_list, unit
+        box3 = (
+            # Left side
+            (box[0] + box[3])/2.0,
+            (box[0] + box[2])/2.0,
+            (box[2] + box[3])/2.0,
+            box[3],
+            # Right side
+            (box[0] + box[7])/2.0,
+            (box[0] + box[6])/2.0,
+            (box[3] + box[6])/2.0,
+            (box[3] + box[7])/2.0,
+        )
+
+        box4 = (
+            # Left side
+            (box[0] + box[4])/2.0,
+            (box[0] + box[5])/2.0,
+            (box[0] + box[6])/2.0,
+            (box[0] + box[7])/2.0,
+            # Right side
+            box[4],
+            (box[4] + box[5])/2.0,
+            (box[4] + box[6])/2.0,
+            (box[4] + box[7])/2.0,
+        )
+
+        box5 = (
+            # Left side
+            (box[0] + box[5])/2.0,
+            (box[1] + box[5])/2.0,
+            (box[1] + box[6])/2.0,
+            (box[0] + box[6])/2.0,
+            # Right side
+            (box[4] + box[5])/2.0,
+            box[5],
+            (box[5] + box[6])/2.0,
+            (box[4] + box[6])/2.0,
+        )
+
+        box6 = (
+            # Left side
+            (box[0] + box[6])/2.0,
+            (box[1] + box[6])/2.0,
+            (box[2] + box[6])/2.0,
+            (box[3] + box[6])/2.0,
+            # Right side
+            (box[4] + box[6])/2.0,
+            (box[5] + box[6])/2.0,
+            box[6],
+            (box[6] + box[7])/2.0,
+        )
+
+        box7 = (
+            # Left side
+            (box[0] + box[7])/2.0,
+            (box[0] + box[6])/2.0,
+            (box[3] + box[6])/2.0,
+            (box[3] + box[7])/2.0,
+            # Right side
+            (box[4] + box[7])/2.0,
+            (box[4] + box[6])/2.0,
+            (box[6] + box[7])/2.0,
+            box[7],
+        )
+        return box0, box1, box2, box3, box4, box5, box6, box7
 
     @staticmethod
-    def _debug_place_points(obj, vec_list, unit):
-        for vec in vec_list:
-            print(vec)
+    def get_bvhtree_from_box(box):
+        mesh_data = bpy.data.meshes.new("cube_mesh_data")
+        faces = [(0, 1, 2, 3),
+                 (4, 7, 6, 5),
+                 (0, 4, 5, 1),
+                 (1, 5, 6, 2),
+                 (2, 3, 7, 6),
+                 (4, 0, 3, 7)]
+        mesh_data.from_pydata([x.to_tuple() for x in box], [], faces)
+        mesh_data.update()
+        bm = bmesh.new()
+        bm.from_mesh(mesh_data)
+        return bvh.BVHTree.FromBMesh(bm)
 
-            import time
-            start = time.time()
-            is_in = Converter.is_inside_polygon(obj.data, vec)
-            elapsed_time = (time.time() - start) * 1000
-            print("is_inside_polyton: elapsed_time:{0}".format(elapsed_time) + "[ms]")
+    @staticmethod
+    def check_if_overlap(obj, box):
+        bvh_tree1 = bvh.BVHTree.FromObject(obj, bpy.context.scene)
+        bvh_tree2 = Converter.get_bvhtree_from_box(box)
+        return bvh_tree1.overlap(bvh_tree2)
 
-
-            if is_in:
-                bpy.ops.mesh.primitive_cube_add(
-                    radius=4,
-                    view_align=False,
-                    enter_editmode=False,
-                    location=vec.to_tuple())
-                bpy.context.active_object.color = (1.0, 0, 0, 1.0)
-                print("is_in")
+    def create_voxel(self, obj, box, depth, max_depth=3):
+        depth += 1
+        overlap = Converter.check_if_overlap(obj, box)
+        if overlap:
+            if depth == max_depth:
+                self.voxel_list.append(box)
             else:
-                bpy.ops.mesh.primitive_cube_add(
-                    radius=1,
-                    view_align=False,
-                    enter_editmode=False,
-                    location=vec.to_tuple())
-                bpy.context.active_object.color = (0, 0, 1.00, 1.0)
+                boxes = Converter.create_new_octree(box)
+                for _child in boxes:
+                    self.create_voxel(obj, _child, depth, max_depth)
 
-    @staticmethod
-    def is_inside_polygon(mesh, point):
-        inf = Vector((1e4, 1e4, 1e4))
-        ray_vector = point - inf
+    def draw_voxel(self):
+        # Add null object
+        e = bpy.data.objects.new("Empty", bpy.data.meshes.new("Empty"))
+        bpy.context.scene.objects.link(e)
 
-        in_ins = {}
-        out_ins = {}
+        for i, voxel in enumerate(self.voxel_list):
+            obj_name = "Cube.%03d" % i
 
-        for i, poly in enumerate(mesh.polygons):
-            vs = []
-            for j in poly.loop_indices:
-                vs.append(mesh.vertices[mesh.loops[j].vertex_index].co)
+            mesh_data = bpy.data.meshes.new("cube_mesh_data")
+            faces = [(0, 1, 2, 3),
+                     (4, 7, 6, 5),
+                     (0, 4, 5, 1),
+                     (1, 5, 6, 2),
+                     (2, 3, 7, 6),
+                     (4, 0, 3, 7)]
+            mesh_data.from_pydata([x.to_tuple() for x in voxel], [], faces)
+            mesh_data.update()
 
-            # Ray Casting
-            result = mg.intersect_ray_tri(vs[0], vs[1], vs[2], ray_vector, inf)
-            if result:
-                dot_val = poly.normal.dot(point - result)
-                key = "{}:{}:{}".format(result[0], result[1], result[2])
-
-                if dot_val < 0:
-                    if key not in in_ins:
-                        in_ins[key] = True
-                else:
-                    if key not in out_ins:
-                        out_ins[key] = True
-
-        # import pprint
-        # print(pprint.pformat(in_ins))
-        # print(pprint.pformat(out_ins))
-        return len(in_ins) > len(out_ins)
+            cube_object = bpy.data.objects.new(obj_name, mesh_data)
+            bpy.context.scene.objects.link(cube_object)
+            cube_object.select = True
+            cube_object.parent = e
 
 
 class MineManager(object):
@@ -180,75 +247,10 @@ class Convert2BlockOperator(bpy.types.Operator):
         obj.modifiers.new("Triangulate", "TRIANGULATE")
         bpy.ops.object.modifier_apply(apply_as="DATA", modifier="Triangulate")
 
-        mesh = obj.data
-        # vcols = mesh.vertex_colors
-        # if len(vcols) == 0:
-        #     vcols.new()
-        # color_layer = vcols["Col"]
-        #
-        # inf_point = Vector((1e5, 1e5, 1e5))
-        # target_point = Vector((-10, -10, -10))
-        # ray_vector = target_point - inf_point
-        #
-        # print(len(mesh.vertices))
-        # for i, poly in enumerate(mesh.polygons):
-        #     print("Polygon number: {}".format(i))
-        #
-        #     v0 = mesh.vertices[mesh.loops[poly.loop_indices[0]].vertex_index].co
-        #     v1 = mesh.vertices[mesh.loops[poly.loop_indices[1]].vertex_index].co
-        #     v2 = mesh.vertices[mesh.loops[poly.loop_indices[2]].vertex_index].co
-        #     # ray = Vector((0.0, 0.0, -1))
-        #     # orig = Vector((-0, -0, 10.0))
-        #     result = mg.intersect_ray_tri(v0, v1, v2, ray_vector, inf_point)
-        #     if result:
-        #         print("result:", result)
-        #         print("normal:", poly.normal)
-        #         print("vec:", target_point - result)
-        #
-        #         dot_val = poly.normal.dot(target_point - result)
-        #         print(dot_val)
-        #
-        #         if dot_val < 0:
-        #             # INTO
-        #             for j in poly.loop_indices:
-        #                 color_layer.data[j].color = 1, 0, 0
-        #         else:
-        #             # OUT
-        #             for j in poly.loop_indices:
-        #                 color_layer.data[j].color = 0, 0, 1
-
-        ############################################
-        # test_v = Vector((-0, -0, -0))
-        # result = Converter.is_inside_polygon(mesh=mesh, point=test_v)
-        # print("test_v({}) is in poly: {}".format(test_v, result))
-        #
-        # test_v = Vector((-0.5, -0, -0))
-        # result = Converter.is_inside_polygon(mesh=mesh, point=test_v)
-        # print("test_v({}) is in poly: {}".format(test_v, result))
-        #
-        # test_v = Vector((-0, -0.5, -0))
-        # result = Converter.is_inside_polygon(mesh=mesh, point=test_v)
-        # print("test_v({}) is in poly: {}".format(test_v, result))
-        #
-        # test_v = Vector((-0, -0, -0.5))
-        # result = Converter.is_inside_polygon(mesh=mesh, point=test_v)
-        # print("test_v({}) is in poly: {}".format(test_v, result))
-        #
-        # test_v = Vector((-10, -0, -0))
-        # result = Converter.is_inside_polygon(mesh=mesh, point=test_v)
-        # print("test_v({}) is in poly: {}".format(test_v, result))
-        import time
-
-        start = time.time()
-        vec_list, unit = Converter.define_grids(obj)
-        elapsed_time = (time.time() - start) * 1000
-        print("elapsed_time:{0}".format(elapsed_time) + "[ms]")
-
-        start = time.time()
-        Converter._debug_place_points(obj, vec_list, unit)
-        elapsed_time = (time.time() - start) * 1000
-        print("elapsed_time:{0}".format(elapsed_time) + "[ms]")
-
+        initial_bb = [Vector(x) for x in obj.bound_box]
+        cvt = Converter()
+        cvt.create_voxel(obj, initial_bb, 0, 4)
+        cvt.draw_voxel()
         return {"FINISHED"}
 
 
